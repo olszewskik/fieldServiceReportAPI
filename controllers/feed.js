36bf485d574d8018ef6f352bfb60d369d3a91bc4
Kamil Olszewski <kamil.web@gmail.com>
2019-10-19 22:22:57 +0200

    add express-validation

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,15 +1,55 @@
+const { validationResult } = require('express-validator/check');
+
 exports.getPosts = (req, res, next) => {
   res.status(200).json({
-    posts: [{ title: 'First Posts', content: 'This is the first post' }],
+    posts: [
+      {
+        name: 'Kamil Olszewski',
+        date: new Date(),
+        placement: 1,
+        video: 0,
+        hours: 1.5,
+        returnVisits: 0,
+        studies: 0,
+      },
+      {
+        name: 'Kamil Olszewski',
+        date: new Date(),
+        placement: 3,
+        video: 1,
+        hours: 3,
+        returnVisits: 2,
+        studies: 0,
+      },
+    ],
   });
 };
 
 exports.createPost = (req, res, next) => {
-  const title = req.body.title;
-  const content = req.body.content;
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res
+      .status(422)
+      .json({
+        message: 'Validation failed, entered data is incorrect',
+        errors: errors.array(),
+      });
+  }
+  const name = req.body.name;
+  const video = req.body.video;
+  const hours = req.body.hours;
+  const returnVisits = req.body.returnVisits;
+  const studies = req.body.studies;
 
   res.status(201).json({
     message: 'Post created successfully!',
-    post: { id: new Date().toISOString(), title: title, content: content },
+    post: {
+      name: name,
+      date: new Date().toISOString(),
+      video: video,
+      hours: hours,
+      returnVisits: returnVisits,
+      studies: studies,
+    },
   });
 };
--- routes/feed.js
+++ routes/feed.js
@@ -1,10 +1,23 @@
 const express = require('express');
+const { body } = require('express-validator/check');
 const feedController = require('../controllers/feed');
 
 const router = express.Router();
 
 router.get('/posts', feedController.getPosts);
 
-router.post('/post', feedController.createPost);
+router.post(
+  '/post',
+  [
+    body('name')
+      .trim()
+      .isString()
+      .isLength({ min: 5 }),
+    body('video')
+      .trim()
+      .isNumeric(),
+  ],
+  feedController.createPost,
+);
 
 module.exports = router;
